cd5d1745bf08b9b324f581ed55add920c8b9492e
princesimform <prince.m@simformsolutions.com>
2023-04-05 13:37:59 +0530

    update html node elements selectors

--- dist/module/Evaluate.js
+++ dist/module/Evaluate.js
@@ -3,8 +3,6 @@ function Evaluate(equ) {
     var infixToPostfix = new InfixToPostfix;
     infixToPostfix.convertInfixToPostFix(equ);
     var pfixVal = infixToPostfix.getPostfix();
-    // history.innerHTML += `<button value=${equ} class="history-item">${equ}</button>`;
-    // history.scrollTo(0, history.scrollHeight)
     var postFix = new PostFix;
     postFix.EvaluatePostFix(pfixVal);
     var EquEvalRes = postFix.getResult();
--- src/module/Evaluate.ts
+++ src/module/Evaluate.ts
@@ -1,18 +1,11 @@
-import { LinkedStack, stackNode, InfixToPostfix, PostFix } from "./EX.js";
-import { output } from "./utils.js";
+import { InfixToPostfix, PostFix } from "./EX.js";
 function Evaluate(equ: string): number | undefined {
     var infixToPostfix = new InfixToPostfix;
     infixToPostfix.convertInfixToPostFix(equ);
     var pfixVal: Array<string> = infixToPostfix.getPostfix();
-
-    // history.innerHTML += `<button value=${equ} class="history-item">${equ}</button>`;
-    // history.scrollTo(0, history.scrollHeight)
-
     var postFix = new PostFix;
-
     postFix.EvaluatePostFix(pfixVal);
     var EquEvalRes: number | null | undefined = postFix.getResult();
-
     if (EquEvalRes != null || EquEvalRes != undefined) {
         return Number(EquEvalRes.toFixed(2));
     }
